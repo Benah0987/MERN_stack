cf489bbf74c49af94c24b6ac920c6d89b47db02c
Benah0987 <wanyoikebenayah77@gmail.com>
2024-06-17 17:05:14 +0300

    authmidddleware

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -37,7 +37,7 @@ const registerUser = asyncHandler(async (req, res) => {
             _id: user.id,
             name: user.name,
             email: user.email,
-            // token: generateToken(user._id),
+            token: generateToken(user._id),
         });
     } else {
         res.status(400);
@@ -49,14 +49,41 @@ const registerUser = asyncHandler(async (req, res) => {
 
 // Login user
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({ message: 'Logged in User' });
+    const {email, password} = req.body
+
+    // check for email
+    const user = await User.findOne({email})
+
+    if(user  && (await bcrypt.compare(password, user.password))){
+        res.json({
+            _id: user.id,
+            name: user.name,
+            token: generateToken(user._id),
+            email: user.email,
+
+        })
+
+    }
+    else{
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+    
 });
 
 // Get current user
+// private
 const getMe = asyncHandler(async (req, res) => {
     res.json({ message: 'User data' });
 });
 
+
+// generate jwt 
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    })
+}
 module.exports = {
     registerUser,
     loginUser,
