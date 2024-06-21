d3b25e0d345b4124be80fdbb3e5913e9693428e1
Benah0987 <wanyoikebenayah77@gmail.com>
2024-06-21 15:56:46 +0300

    this is good

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 
 function App() {
   return (
-    <div className="App">
+    <>
 
-      <h1>so slow</h1>
-    </div>
+      <Router>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element ={<Dashboard/>} />
+            <Route path='/login' element ={<Login/>} />
+            <Route path='/register' element ={<Register/>} />
+
+          </Routes>
+
+        </div>
+      </Router>
+    </>
   );
 }
 
